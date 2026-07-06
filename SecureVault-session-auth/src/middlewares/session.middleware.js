b41fb7ca1ee6a89a21fef8fc281c1c400d151086
raghuwar-jangir <raghujangid222@gmail.com>
2026-07-06 14:38:39 +0530

    implement session rolling

--- SecureVault-session-auth/src/middlewares/session.middleware.js
+++ SecureVault-session-auth/src/middlewares/session.middleware.js
@@ -6,8 +6,9 @@ const sessionMiddleware = () =>
   session({
     secret: process.env.SECRET,
     name: "svid",
+    rolling: true, //extend session if visited withing expiry
     cookie: {
-      maxAge: 2 * 60 * 60 * 1000, //2 hours
+      maxAge: 30 * 1000, //2 hours
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // because our frontend and backend are different origin,
       // thats why we have CORS. so "none" is right here
